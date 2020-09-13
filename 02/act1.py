c6ded978c5ee6d781ec8c76c5150b3c07a15d904
Michael Vaughan <mavaughan@protonmail.com>
2020-09-13 19:12:05 -0400

    consistent output

--- 02/act1.py
+++ 02/act1.py
@@ -2,5 +2,5 @@
 
 from simplehttp import SimpleHTTPRequest
 req = SimpleHTTPRequest(method="POST")
-flag = req.request().split("\r\n")[-1].split("is ")[-1]
+flag = req.request().split("\r\n")[-1].split("is ")[-1][:-1]
 print(flag)
